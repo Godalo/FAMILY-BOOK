db9d5a2762ff2e27a431c1a08b55f3fe7b9be928
Godalo <81340015+Godalo@users.noreply.github.com>
2021-04-01 11:39:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function change(){
+
+    var names=["maa","papa","Bhoomi","daadi","tai","priyanshi","daizy"];
+
+    var images=["mom.jpg","men.jfif","me.jfif","daadi.jfif","tai.jfif","pallu.jfif","daizy.jfif "];
+
+    document.getElementsByName("maa").value;
+    console.log(change);
+
+}
